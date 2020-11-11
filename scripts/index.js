025beba2afa2ad75e82de43ecce298854b8c00dd
AlexMrgt <git.mrgt@gmail.com>
2020-11-11 20:36:50 +0500

    fix review

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ let editModal = document.querySelector(".popup");
 let editForm = editModal.querySelector(".popup__form");
 let editName = editForm.querySelector(".popup__edit-field_scope_name");
 let editDescription = editForm.querySelector(".popup__edit-field_scope_description");
-let editSubmitButton = editForm.querySelector(".popup__save-button");
 let editCloseButton = editForm.querySelector(".popup__close-button");
 
 let profile = document.querySelector(".profile");
@@ -13,12 +12,9 @@ let profileName = profile.querySelector(".profile__name");
 let profileDescription = profile.querySelector(".profile__description");
 let profileEditButton = profile.querySelector(".profile__edit-button");
 
-/* Есть проблема длинных имен и описаний: если слишком длинные - то это убого выглядит.
-
-      Думаю, что можно по условию >15 смиволов переносить на другую строку часть текста,
-      вставляя символ переноса строки, но для этого надо как-то вытащить строку и сделать
-      вставку, пока не знаю, как это реализовать.
-  */
+/* Этот коменнтарий относился к секции profile. Если вводить слишком
+    длинные имена и описания, то вся верска может уехать.
+*/
 
 function openModal(modal) {
 
@@ -27,11 +23,7 @@ function openModal(modal) {
   modal.classList.add("popup_active");
   overlay.classList.add("overlay_active");
 
-  // проверка на конкретный попап, не во всех нужно будет дефолтные значения прописывать
-  if (modal.classList.contains('popup')) {
-
-    setFormDefaultValues();
-  }
+  setFormDefaultValues();
 }
 
 function setFormDefaultValues() {
