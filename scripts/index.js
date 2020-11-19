cecc3e8b6d838f947ba0b6a5c0813fc78e6fc257
AlexMrgt <git.mrgt@gmail.com>
2020-11-19 16:02:54 +0500

    add add-card delete-card card-popup like-button

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,90 @@
+const overlay = document.querySelector(".overlay");
 
-let overlay = document.querySelector(".overlay");
+const editModal = document.querySelector(".popup_scope_edit");
+const editForm = editModal.querySelector(".popup__form_scope_edit");
+const editName = editForm.querySelector(".popup__form-field_scope_name");
+const editDescription = editForm.querySelector(".popup__form-field_scope_description");
+const editCloseButton = editForm.querySelector(".popup__close-popup");
 
-let editModal = document.querySelector(".popup");
-let editForm = editModal.querySelector(".popup__form");
-let editName = editForm.querySelector(".popup__edit-field_scope_name");
-let editDescription = editForm.querySelector(".popup__edit-field_scope_description");
-let editCloseButton = editForm.querySelector(".popup__close-button");
+const addModal =  document.querySelector(".popup_scope_add");
+const addForm = addModal.querySelector(".popup__form_scope_add");
+const addPlaceName = addForm.querySelector(".popup__form-field_scope_place-name");
+const addPlaceUrl = addForm.querySelector(".popup__form-field_scope_place-url");
+const addCloseButton = addForm.querySelector(".popup__close-popup");
 
-let profile = document.querySelector(".profile");
-let profileName = profile.querySelector(".profile__name");
-let profileDescription = profile.querySelector(".profile__description");
-let profileEditButton = profile.querySelector(".profile__edit-button");
+const cardModal = document.querySelector(".popup_scope_picture");
+const cardModalPicture = cardModal.querySelector(".card-popup__image");
+const cardModalCaption = cardModal.querySelector(".card-popup__caption");
+const cardModalClose = cardModal.querySelector(".card-popup__close-popup");
 
-/* Этот коменнтарий относился к секции profile. Если вводить слишком
-    длинные имена и описания, то вся верска может уехать.
+const profile = document.querySelector(".profile");
+const profileName = profile.querySelector(".profile__name");
+const profileDescription = profile.querySelector(".profile__description");
+const profileEditButton = profile.querySelector(".profile__edit-profile");
+const profileAddButton = profile.querySelector(".profile__add-card");
+
+const gallery = document.querySelector(".gallery");
+
+const cardTemplate =  document.querySelector('#card-template').content;
+
+const defaultCardsProperties = [
+  {
+    caption:" Гора Эльбрус",
+    url: "./images//gallery/Эльбрус.jpg",
+    alt:"Фото горы Эльбрус"
+  },
+
+  {
+    caption:"Аракульские Шиханы",
+    url: "./images/gallery/Шиханы.jpg",
+    alt: "Фото Аракульских Шиханов"
+  },
+
+  {
+    caption:"Джейъаско-Ассинский заповедник",
+    url: "./images/gallery/Джейъаско-Ассинский_заповедник.jpg",
+    alt: "Фото Джейъаско-Ассинского заповедника",
+  },
+
+  {
+    caption: "Ключевая сопка",
+    url: "./images/gallery/Ключевая_сопка.jpg",
+    alt: "Фото вулкана Ключевая Сопка"
+  },
+
+  {
+    caption: "Кунгурская_пещера",
+    url: "./images/gallery/Кунгурская_пещера.jpg",
+    alt: "Фото Кунгурской пещеры"
+  },
+
+  {
+    caption: "Ленские столбы",
+    url: "./images/gallery/Ленские_столбы.jpg",
+    alt: "Фото Ленских столбов"
+  },
+]
+
+/*С именованием ф-ий/переменных не очень понятно: слово add обычно используется
+  именно как описание фунционала, т.е. ф-я что-то добавляет,
+  а тут add/edit - это просто идентификатор попапа,
+  мне кажется, это неправильно, но не знаю как иначе.
 */
 
 function openModal(modal) {
 
-  if (modal == null) return;
-
-  modal.classList.add("popup_active");
   overlay.classList.add("overlay_active");
+  modal.classList.add("popup_active");
 
-  setFormDefaultValues();
+  if (modal.classList.contains("popup_scope_edit"))
+    setFormDefaultValues();
+}
+
+function closeModal(modal) {
+
+  overlay.classList.remove("overlay_active");
+  modal.classList.remove("popup_active");
+
 }
 
 function setFormDefaultValues() {
@@ -32,13 +93,7 @@ function setFormDefaultValues() {
   editDescription.value = profileDescription.textContent;
 }
 
-function closeModal(modal) {
-
-  modal.classList.remove("popup_active");
-  overlay.classList.remove("overlay_active");
-}
-
-function formSubmitHandler(evt) {
+function editFormSubmitHandler(evt) {
 
   evt.preventDefault();
 
@@ -48,14 +103,103 @@ function formSubmitHandler(evt) {
   closeModal(editModal);
 }
 
+function addFormSubmitHandler(evt){
+
+  evt.preventDefault();
+
+  renderCard(createCard(addPlaceUrl.value, addPlaceName.value));
+
+  closeModal(addModal);
+}
+
+function createCard(sourse, title, alternative = title){
+
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__picture").src = sourse;
+  card.querySelector(".card__picture").alt = alternative;
+  card.querySelector(".card__title").textContent = title;
+
+  card.querySelector(".card__like").addEventListener('click', evt => {
+
+    evt.target.classList.toggle('card__like_active');
+  });
+
+  /* Не уверен, что удаление карточки должно осуществляться таким образом,
+    то выглядит не универсально, что если родителем кнопки
+    будет не сама карточка, а какой-то div на ней
+  */
+  card.querySelector(".card__delete-card").addEventListener('click', evt => {
+
+    evt.target.parentElement.remove();
+
+    if (!gallery.querySelector(".card")) {
+      renderNoCards();
+    }
+
+  });
+
+  card.querySelector(".card__picture").addEventListener("click", () => {
+
+    openModal(cardModal);
+    setCardPopupContent(sourse, title);
+  })
+
+  return card;
+}
+
+function renderCard(card){
+
+  gallery.prepend(card);
+  renderHasCards();
+}
+
+function renderDefaultCards(defaultSourses){
+
+  defaultSourses.forEach( sourse => {
+    renderCard(createCard(sourse.url, sourse.caption, sourse.alt));
+  })
+}
+
+function renderNoCards(){
+
+  gallery.querySelector('.no-cards').classList.remove("no-cards_hidden");
+}
+
+function renderHasCards(){
+
+  gallery.querySelector('.no-cards').classList.add("no-cards_hidden");
+}
+
+function setCardPopupContent(src, caption){
+
+  cardModalPicture.src = src;
+  cardModalCaption.textContent = caption;
+}
+
+renderDefaultCards(defaultCardsProperties);
+
 profileEditButton.addEventListener("click", () => {
 
   openModal(editModal);
 })
-
 editCloseButton.addEventListener("click", () => {
 
   closeModal(editModal);
 })
+editForm.addEventListener("submit", editFormSubmitHandler);
 
-editForm.addEventListener("submit", formSubmitHandler);
+profileAddButton.addEventListener("click", () => {
+
+  openModal(addModal);
+})
+addCloseButton.addEventListener("click", () => {
+
+  closeModal(addModal);
+})
+addForm.addEventListener("submit", addFormSubmitHandler);
+
+cardModalClose.addEventListener("click", () => {
+
+  closeModal(cardModal);
+})
